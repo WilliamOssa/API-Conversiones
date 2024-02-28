cb340b1dd1246a72da916651746ee01631e90839
William <william.ossa@serverli.com>
2024-02-28 16:10:51 -0500

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.port;
+
 const {convertTemperature} = require('./functionHandler');
-const functionHandler = require('./functionHandler');
 
 app.use(express.static('views'));
 
