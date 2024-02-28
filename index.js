9c003aa5e27a614573251dfae9fe07f12a54a282
William <william.ossa@serverli.com>
2024-02-28 14:49:39 -0500

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.port;
 
 app.use(express.static('views'));
 
